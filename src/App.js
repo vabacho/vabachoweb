6a38984d18daa7670f1f66820d4fafd13d53db61
Chandan Kapilavai <ckapilavai@web.com>
2019-10-17 18:43:35 +0530

    added meterial ui dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'typeface-roboto';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 function App() {
+
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
   return (
+    <React.Fragment>
+
+<Card className={classes.card}>
+    <CardContent>
+      <Typography className={classes.title} color="textSecondary" gutterBottom>
+        Vabacho Development in progress
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small" color="primary" variant="contained">Learn More</Button>
+    </CardActions>
+    </Card>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +60,7 @@ function App() {
         </a>
       </header>
     </div>
+</React.Fragment>
   );
 }
 
